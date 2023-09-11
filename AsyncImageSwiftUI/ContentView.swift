ef71a41fbbc6afb6ffb17ea9c544d856687907b4
erenolkun <erenolkun@gmail.com>
2023-09-11 16:43:16 +0300

    the end of proje

--- AsyncImageSwiftUI/ContentView.swift
+++ AsyncImageSwiftUI/ContentView.swift
@@ -9,18 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            
+            VStack{
+                AsyncImage(url: URL(string: "https://media.licdn.com/dms/image/D4D03AQF7QrA4QVMB_w/profile-displayphoto-shrink_800_800/0/1689141207893?e=2147483647&v=beta&t=LbZa_p83Uz_hjhpewEGFhVOA1ObqqEdiZlbsf3jXw7Y")!) { image in
+                    image.resizable().frame(width: 300, height: 300, alignment: .center)
+//Görsel indirilene kadar ne gösterilsin
+                } placeholder: {
+                    ProgressView()
+                }
+
+                List(superHeroArray){
+                    superhero in Text(superhero.name)
+                        .font(.title3)
+                        .foregroundColor(.black)
+                }
+            }.navigationTitle(Text("Superhero Book"))
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
